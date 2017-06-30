047277c43560102c87e923c55d4fffb3c83035c8
XCODE <xcode@ENGs-iMac.local>
2017-06-30 08:42:30 -0300

    Initial Commit

---
+++ Sensores/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sensores
+//
+//  Created by XCODE on 30/06/17.
+//  Copyright © 2017 XCODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sensores/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sensores
+//
+//  Created by XCODE on 30/06/17.
+//  Copyright © 2017 XCODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
